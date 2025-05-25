0a9aace0a4fa85bd23af22eeee0e6b2d96b13d7e
praveensivasakthi <praveenkumar080204@gmail.com>
2025-05-25 15:04:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Lazada callback server is live!'
+    return 'Lazada Callback App Running'
 
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    return f'Authorization code received: {code}', 200
+    if code:
+        return f'Authorization Code received: {code}'
+    else:
+        return 'No authorization code received', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
